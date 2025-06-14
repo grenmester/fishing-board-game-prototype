6147a12895bc844b0f543f91b5f2b496487ca16a
Jacky Lee <dr.jackylee@gmail.com>
2025-06-14 21:02:41 +0800

    feat: add join and start game functionality

--- ws-proto/client/src/App.jsx
+++ ws-proto/client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { FaCircleUser, FaFish, FaMoneyBill, FaStar } from "react-icons/fa6";
 import {
   GiInvertedDice1,
   GiInvertedDice2,
@@ -23,7 +24,10 @@ const diceIcons = [
 ];
 
 const App = () => {
+  const [gameInProgress, setGameInProgress] = useState(false);
+  const [playerId, setPlayerId] = useState(-1);
   const [gameState, setGameState] = useState({
+    players: [],
     numDice: 1,
     diceRolls: [0],
   });
@@ -39,7 +43,13 @@ const App = () => {
       const data = JSON.parse(e.data);
 
       switch (data.type) {
-        case "update":
+        case "gameInProgress":
+          setGameInProgress(data.gameInProgress);
+          break;
+        case "playerId":
+          setPlayerId(data.playerId);
+          break;
+        case "gameState":
           setGameState(data.gameState);
           break;
         case "error":
@@ -59,42 +69,89 @@ const App = () => {
 
   return (
     <div className="flex flex-col gap-y-4 items-center p-4 sm:p-10">
-      <h1 className="flex justify-center m-10 text-4xl font-bold text-center">
+      <h1 className="flex justify-center m-10 text-6xl font-bold text-center">
         Fishing Board Game Prototype
       </h1>
-      <div className="p-4 w-full max-w-md bg-gray-100 rounded-xl">
-        <h2 className="mb-4 font-bold text-center">Dice</h2>
+      <p>{gameInProgress ? "Game in Progress" : "Waiting for Game to Start"}</p>
+      {!gameInProgress && (
+        <div className="flex gap-x-4">
+          {playerId == -1 && (
+            <button
+              className="py-2 px-4 bg-purple-300 rounded-full"
+              onClick={() => sendMessage({ type: "joinGame" })}
+            >
+              Join Game
+            </button>
+          )}
+          <button
+            className="py-2 px-4 bg-purple-300 rounded-full"
+            onClick={() => sendMessage({ type: "startGame" })}
+          >
+            Start Game
+          </button>
+        </div>
+      )}
+      <div className="p-4 w-full max-w-4xl bg-purple-100 rounded-xl">
+        <h2 className="mb-4 text-4xl font-bold text-center">Players</h2>
+        <div className="flex flex-wrap gap-4 justify-center">
+          {gameState.players.map((player, idx) => (
+            <div
+              className={`flex flex-col gap-y-2 items-center p-4 bg-purple-300 rounded-lg ${playerId == idx && "border-2 border-purple-700"}`}
+              key={idx}
+            >
+              <div className="flex gap-4">
+                <div className="flex gap-1 items-center">
+                  <FaStar className="text-yellow-500" />
+                  <span>{player.reputation}</span>
+                </div>
+                <div className="flex gap-1 items-center">
+                  <FaMoneyBill className="text-green-500" />
+                  <span>{player.money}</span>
+                </div>
+                <div className="flex gap-1 items-center">
+                  <FaFish className="text-blue-500" />
+                  <span>{player.hand.length}</span>
+                </div>
+              </div>
+              <FaCircleUser size={48} />
+              <p className="font-bold">Player {idx}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="p-4 w-full max-w-2xl bg-gray-100 rounded-xl">
+        <h2 className="mb-4 text-4xl font-bold text-center">Dice</h2>
         <div className="flex flex-col gap-y-2 items-center">
           <div className="flex gap-x-2">
             {gameState.diceRolls.map((roll, idx) => {
               const Icon = diceIcons[roll] || GiSquare;
-              return <Icon key={idx} size={32} />;
+              return <Icon key={idx} size={48} />;
             })}
           </div>
-          <div className="flex gap-x-2">
+          <div className="flex gap-x-2 text-white">
             <button
               className="p-2 bg-gray-500 rounded-full"
               onClick={() => sendMessage({ type: "addDie" })}
             >
-              <IoMdAdd className="text-white" />
+              <IoMdAdd size={24} />
             </button>
             <button
               className="p-2 bg-gray-500 rounded-full"
               onClick={() => sendMessage({ type: "removeDie" })}
             >
-              <IoMdRemove className="text-white" />
+              <IoMdRemove size={24} />
             </button>
             <button
               className="p-2 bg-gray-500 rounded-full"
               onClick={() => sendMessage({ type: "rollDice" })}
             >
-              <IoMdSync className="text-white" />
+              <IoMdSync size={24} />
             </button>
           </div>
         </div>
       </div>
       <div className="p-4 w-full max-w-4xl bg-yellow-100 rounded-xl">
-        <h2 className="mb-4 font-bold text-center">Game State</h2>
+        <h2 className="mb-4 text-4xl font-bold text-center">Game State</h2>
         <pre>{JSON.stringify(gameState, null, 2)}</pre>
       </div>
       {error && (
--- ws-proto/server/server.js
+++ ws-proto/server/server.js
@@ -2,17 +2,10 @@ import { WebSocketServer } from "ws";
 
 const PORT = 3000;
 
-const genInitialGameState = () => {
-  return {
-    numDice: 1,
-    diceRolls: [0],
-  };
-};
-
-const broadcastGameState = () => {
+const broadcast = (message) => {
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({ type: "update", gameState }));
+      client.send(JSON.stringify(message));
     }
   });
 };
@@ -23,20 +16,58 @@ const sendError = (socket, error) => {
 };
 
 const wss = new WebSocketServer({ port: PORT });
-let gameState = genInitialGameState();
+let gameInProgress = false;
+let gameState = {
+  players: [],
+  numDice: 1,
+  diceRolls: [0],
+};
 
 wss.on("connection", (ws) => {
   console.info("Client connected");
+  broadcast({ type: "gameInProgress", gameInProgress });
+  broadcast({ type: "gameState", gameState });
 
   ws.on("message", (message) => {
     const data = JSON.parse(message.toString());
     console.debug(`message: ${JSON.stringify(data, null, 2)}`);
+
     switch (data.type) {
+      case "joinGame":
+        if (gameInProgress) {
+          sendError(ws, "Game is already in progress");
+          return;
+        }
+
+        ws.playerId = gameState.players.length;
+        gameState.players.push({
+          hand: [],
+          reputation: 0,
+          money: 0,
+        });
+        ws.send(JSON.stringify({ type: "playerId", playerId: ws.playerId }));
+        break;
+      case "startGame":
+        if (gameInProgress) {
+          sendError(ws, "Game is already in progress");
+          return;
+        }
+
+        const numPlayers = gameState.players.length;
+        if (numPlayers < 2 || numPlayers > 8) {
+          sendError(ws, "Number of players must be between 2 and 8");
+          return;
+        }
+
+        gameInProgress = true;
+        broadcast({ type: "gameInProgress", gameInProgress });
+        break;
       case "addDie":
         if (gameState.numDice == 10) {
           sendError(ws, "Max number of dice is 10");
           return;
         }
+
         gameState.numDice++;
         gameState.diceRolls.push(0);
         break;
@@ -45,6 +76,7 @@ wss.on("connection", (ws) => {
           sendError(ws, "Min number of dice is 1");
           return;
         }
+
         gameState.numDice--;
         gameState.diceRolls.pop();
         break;
@@ -54,12 +86,21 @@ wss.on("connection", (ws) => {
           .map(() => Math.floor(Math.random() * 6) + 1);
         break;
     }
-    broadcastGameState();
+    broadcast({ type: "gameState", gameState });
   });
 
   ws.on("close", () => {
-    console.info("Client disconnected, game reset");
-    gameState = genInitialGameState();
+    console.info("Client disconnected");
+
+    if (!("playerId" in ws)) {
+      return;
+    }
+
+    console.info("Game reset");
+    gameInProgress = false;
+    gameState.players = [];
+    broadcast({ type: "gameInProgress", gameInProgress });
+    broadcast({ type: "playerId", playerId: -1 });
   });
 });
 
